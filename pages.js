b9b57e0e000f4a7430124288d0e0d0ce14722cdb
Aditya <96467341+adityaprakash-dev@users.noreply.github.com>
2023-09-17 03:23:12 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,1 +1,46 @@
+//this is the main page.js
+'use client'
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+
+const Page = () => {
+  const [users, setusers] = useState([]);
+  
+  const getData = async () =>{
+      const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setusers(data);
+  }
+
+
+  const renderData = (data) => {
+    return(
+      <ul>
+        {data.map((e) => (
+          <li>{e.name} --- <a href={`/${e.id}`}>explore</a></li>
+
+        ))}
+      </ul>
+    )
+  }
+
+  
+  return (
+    <div className='p-5'>
+      
+      <button 
+      onClick={getData}
+      className='bg-slate-950 text-white px-6 py-2 rounded-lg'
+      >Get Data</button>
+      
+      <div className='mt-5 text-black'>
+        {users.length > 0 ? renderData(users) : "Data not fetched yet"} 
+      </div>
+    </div>
+  )
+}
+
+export default Page
+
 
