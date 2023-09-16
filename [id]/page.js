774b24bc23f01cd60d1184a3e626fd96c362235a
Aditya <96467341+adityaprakash-dev@users.noreply.github.com>
2023-09-17 03:29:06 +0530

    Update page.js

--- [id]/page.js
+++ [id]/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//params is a special type of props, that is automatcally passed by next for dynamic routing
 const page = ({params}) => {
   const {id} = params;
   return (
